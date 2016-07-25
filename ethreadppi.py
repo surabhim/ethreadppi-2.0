e80852f31d2e7f8aa038a75c0239aaafdcb84688
surabhim <surabhimaheshwari89@gmail.com>
2016-07-25 11:35:06 -0500

    rename

--- ethreadppi.py
+++ ethreadppi.py
@@ -8,11 +8,11 @@ from sklearn.ensemble import RandomForestClassifier
 ## Usage example :
 # python ethread-2.0-efindsiteppi.py 12asB-hhsearch.out 12asB-raptorx.rank 12asB-sparksx.zs12 0 ./model_data/model_mcc_homo.pkl ./model_data/model_isc_homo.pkl
 if len(sys.argv) != 7:
-    print "USAGE : python sys.argv[0] <hhsearch>"
-    print "                            <raptorx>"  
-    print "                            <sparksx>"
-    print "                <1/0> homo:0 , hete:1"
-    print "                      <rfc_model_mcc>"
+    print "USAGE : python ethreadppi.py <hhsearch>"
+    print "                              <raptorx>"  
+    print "                              <sparksx>"
+    print "                   <1/0> homo:0 , hete:1"
+    print "                         <rfc_model_mcc>"
     print "                      <rfc_model_isc>"
     sys.exit (1)
     
